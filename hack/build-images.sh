50728926e4209c727aeb4d95a7e1f129ad6e3dfe
Ryuko <ryuko@peek1e.eu>
2023-07-05 22:05:25 +0200

    update: dockerfiles

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -m
+
+printf "Building Images"
+printf "%-*s\n" "25" | tr ' ' '-'
+
 VERSION="${VERSION:-dev}"
 
 docker build -t "docker.io/peek1e/file-api:$VERSION" -f src/docker/Dockerfile-api . &
 docker build -t "docker.io/peek1e/file-web:$VERSION" -f src/docker/Dockerfile-web . &
 docker build -t "docker.io/peek1e/file-cleaner:$VERSION" -f src/docker/Dockerfile-cleaner . &
-docker build -t "docker.io/peek1e/file-migration:$VERSION" -f src/docker/Dockerfile-migration . &
+docker build -t "docker.io/peek1e/file-migrations:$VERSION" -f src/docker/Dockerfile-migration . &
+
+while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
+
+printf "%-*s\n" "25" | tr ' ' '-'
+printf "Done :)\n"
